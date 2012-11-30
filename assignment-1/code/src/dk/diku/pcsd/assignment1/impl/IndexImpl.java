291869fb699a2b4dcc6199d7a52a7b7f5f2e73b0
Marco <marco@marco-ThinkPad-T61>
2012-11-30 21:55:55 +0100

    Basic (unsynchronized) functionality for the Index, initial implementations for Key, Value and the Serializer that may or may not work

--- assignment-1/code/src/dk/diku/pcsd/assignment1/impl/IndexImpl.java
+++ assignment-1/code/src/dk/diku/pcsd/assignment1/impl/IndexImpl.java
@@ -1,7 +1,12 @@
 package dk.diku.pcsd.assignment1.impl;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import dk.diku.pcsd.keyvaluebase.exceptions.BeginGreaterThanEndException;
 import dk.diku.pcsd.keyvaluebase.exceptions.KeyAlreadyPresentException;
@@ -9,34 +14,136 @@ import dk.diku.pcsd.keyvaluebase.exceptions.KeyNotFoundException;
 import dk.diku.pcsd.keyvaluebase.interfaces.Index;
 import dk.diku.pcsd.keyvaluebase.interfaces.Pair;
 
-public class IndexImpl implements Index<KeyImpl,ValueListImpl>
-{
+public class IndexImpl implements Index<KeyImpl, ValueListImpl> {
 
-	@Override
+	private StoreImpl store;
+
+	private ValueSerializerImpl vs = new ValueSerializerImpl();
+
+	private int fileLength = 0;
+
+	private List<SpaceIdent> emptyList = Collections.synchronizedList(new LinkedList<SpaceIdent>());
+
+	private Map<KeyImpl, SpaceIdent> mappings = new Hashtable<KeyImpl, SpaceIdent>();
+	
+	private IndexImpl(){
+		this.store=StoreImpl.getInstance();
+	}
+
+	private SpaceIdent findFreeSpace(int length) {
+		SpaceIdent result = null;
+		for (Iterator<SpaceIdent> i = emptyList.iterator(); i.hasNext();) {
+			SpaceIdent current = i.next();
+			if (current.getLength() >= length) {
+				result = current;
+				i.remove();
+				emptyList.add(new SpaceIdent(current.getPos() + length, current
+						.getLength() - length));
+				break;
+			}
+		}
+
+		result = new SpaceIdent(fileLength, length);
+		fileLength += length;
+
+		return result;
+	}
+
+	private void freeSpace(SpaceIdent s) {
+		boolean done = false;
+
+		for (Iterator<SpaceIdent> i = emptyList.iterator(); i.hasNext()
+				&& !done;) {
+			SpaceIdent current = i.next();
+
+			if (s.getPos() == (current.getPos() + current.getLength())) {
+				current.setLength(current.getLength() + s.getLength());
+				s = current;
+				done = true;
+			}
+
+			if (current.getPos() == s.getPos() + s.getLength()) {
+				current.setPos(s.getPos());
+				current.setLength(current.getLength() + s.getLength());
+				s = current;
+				done = true;
+			}
+		}
+
+		if (!done)
+			emptyList.add(s);
+	}
+
 	public void insert(KeyImpl k, ValueListImpl v)
 			throws KeyAlreadyPresentException, IOException {
-		// TODO Auto-generated method stub
-		
+		if (mappings.containsKey(k)) {
+			throw new KeyAlreadyPresentException(k);
+		}
+
+		byte[] toWrite = vs.toByteArray(v);
+
+		SpaceIdent space = findFreeSpace(toWrite.length);
+
+		store.write(space.getPos(), toWrite);
+
+		int ldiff = space.getLength() - toWrite.length;
+
+		if (ldiff > 0)
+			emptyList
+					.add(new SpaceIdent(space.getPos() + toWrite.length, ldiff));
+
+		mappings.put(k, space);
 	}
 
-	@Override
 	public void remove(KeyImpl k) throws KeyNotFoundException {
-		// TODO Auto-generated method stub
-		
+		SpaceIdent s = mappings.get(k);
+
+		if (s == null) {
+			throw new KeyNotFoundException(k);
+		} else {
+			freeSpace(s);
+			mappings.remove(k);
+		}
 	}
 
-	@Override
-	public ValueListImpl get(KeyImpl k) throws KeyNotFoundException,
-			IOException {
-		// TODO Auto-generated method stub
-		return null;
+	public ValueListImpl get(KeyImpl k)
+			throws KeyNotFoundException, IOException {
+		SpaceIdent s = mappings.get(k);
+
+		if (s == null) {
+			throw new KeyNotFoundException(k);
+		} else {
+			byte[] read = store.read(s.getPos(), s.getLength());
+			return vs.fromByteArray(read);
+		}
 	}
 
-	@Override
-	public void update(KeyImpl k, ValueListImpl v) throws KeyNotFoundException,
-			IOException {
-		// TODO Auto-generated method stub
-		
+	public void update(KeyImpl k, ValueListImpl v)
+			throws KeyNotFoundException, IOException {
+		SpaceIdent s = mappings.get(k);
+
+		if (s == null) {
+			throw new KeyNotFoundException(k);
+		} else {
+			byte[] toWrite = vs.toByteArray(v);
+
+			int ldiff = s.getLength() - toWrite.length;
+
+			if (ldiff < 0) {
+				freeSpace(s);
+				s = findFreeSpace(toWrite.length);
+				store.write(s.getPos(), toWrite);
+				mappings.put(k, s);
+			} else {
+				store.write(s.getPos(), toWrite);
+				if (ldiff > 0) {
+					emptyList.add(new SpaceIdent(s.getPos() + toWrite.length,
+							ldiff));
+				}
+				s.setLength(toWrite.length);
+			}
+		}
+
 	}
 
 	@Override
@@ -57,7 +164,7 @@ public class IndexImpl implements Index<KeyImpl,ValueListImpl>
 	public void bulkPut(List<Pair<KeyImpl, ValueListImpl>> keys)
 			throws IOException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
--- assignment-1/code/src/dk/diku/pcsd/assignment1/impl/ValueSerializerImpl.java
+++ assignment-1/code/src/dk/diku/pcsd/assignment1/impl/ValueSerializerImpl.java
@@ -1,6 +1,11 @@
 package dk.diku.pcsd.assignment1.impl;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 
 import dk.diku.pcsd.keyvaluebase.interfaces.ValueSerializer;
 
@@ -8,14 +13,34 @@ public class ValueSerializerImpl implements ValueSerializer<ValueListImpl> {
 
 	@Override
 	public ValueListImpl fromByteArray(byte[] array) throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		ByteArrayInputStream byteStream = new ByteArrayInputStream(array);
+		ObjectInputStream is = new ObjectInputStream(byteStream);
+		
+		Object result = null;
+		try {
+			result = is.readObject();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		if (result instanceof ValueListImpl){
+			return (ValueListImpl) result;
+		}else{
+			return null;
+		}
 	}
 
 	@Override
 	public byte[] toByteArray(ValueListImpl v) throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+		ObjectOutputStream os = new ObjectOutputStream(byteStream);
+		os.writeObject(v);
+		
+		os.close();
+		
+		byte[] result = byteStream.toByteArray();
+		
+		return result;
 	}
 
 }
