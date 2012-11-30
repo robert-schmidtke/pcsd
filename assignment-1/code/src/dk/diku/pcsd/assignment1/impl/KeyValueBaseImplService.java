1314fdf2de2df88627f04a8c46cbaa3408d941e0
Robert Schmidtke <ro.schmidtke@gmail.com>
2012-11-30 20:25:36 +0100

    wrapping key value base impl in extra webservice to avoid duplicates in wsdl

--- assignment-1/code/src/dk/diku/pcsd/assignment1/impl/KeyValueBaseImplService.java
+++ assignment-1/code/src/dk/diku/pcsd/assignment1/impl/KeyValueBaseImplService.java
@@ -4,85 +4,88 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
 import dk.diku.pcsd.keyvaluebase.exceptions.BeginGreaterThanEndException;
 import dk.diku.pcsd.keyvaluebase.exceptions.KeyAlreadyPresentException;
 import dk.diku.pcsd.keyvaluebase.exceptions.KeyNotFoundException;
 import dk.diku.pcsd.keyvaluebase.exceptions.ServiceAlreadyInitializedException;
 import dk.diku.pcsd.keyvaluebase.exceptions.ServiceInitializingException;
 import dk.diku.pcsd.keyvaluebase.exceptions.ServiceNotInitializedException;
-import dk.diku.pcsd.keyvaluebase.interfaces.KeyValueBase;
 import dk.diku.pcsd.keyvaluebase.interfaces.Pair;
 import dk.diku.pcsd.keyvaluebase.interfaces.Predicate;
 
-public class KeyValueBaseImpl implements KeyValueBase<KeyImpl,ValueListImpl>
-{
+@WebService
+public class KeyValueBaseImplService extends KeyValueBaseImpl {
+	
+	public KeyValueBaseImplService() {
+		super();
+	}
 
-	public KeyValueBaseImpl() {
+	public KeyValueBaseImplService(IndexImpl index) {
+		super(index);
 	}
 	
-	public KeyValueBaseImpl(IndexImpl index) {
-		// TODO Auto-generated constructor stub
-	}
-
 	@Override
+	@WebMethod
 	public void init(String serverFilename)
 			throws ServiceAlreadyInitializedException,
 			ServiceInitializingException, FileNotFoundException {
-		// TODO Auto-generated method stub
-		
+		super.init(serverFilename);
 	}
 
 	@Override
+	@WebMethod
 	public ValueListImpl read(KeyImpl k) throws KeyNotFoundException,
 			IOException, ServiceNotInitializedException {
-		// TODO Auto-generated method stub
-		return null;
+		return super.read(k);
 	}
 
 	@Override
+	@WebMethod
 	public void insert(KeyImpl k, ValueListImpl v)
 			throws KeyAlreadyPresentException, IOException,
 			ServiceNotInitializedException {
-		// TODO Auto-generated method stub
-		
+		super.insert(k, v);
 	}
 
 	@Override
+	@WebMethod
 	public void update(KeyImpl k, ValueListImpl newV)
 			throws KeyNotFoundException, IOException,
 			ServiceNotInitializedException {
-		// TODO Auto-generated method stub
-		
+		super.update(k, newV);
 	}
 
 	@Override
+	@WebMethod
 	public void delete(KeyImpl k) throws KeyNotFoundException,
 			ServiceNotInitializedException {
-		// TODO Auto-generated method stub
-		
+		super.delete(k);
 	}
 
 	@Override
+	@WebMethod
 	public List<ValueListImpl> scan(KeyImpl begin, KeyImpl end, Predicate<ValueListImpl> p)
 			throws IOException, BeginGreaterThanEndException,
 			ServiceNotInitializedException {
-		// TODO Auto-generated method stub
-		return null;
+		return super.scan(begin, end, p);
 	}
 
 	@Override
+	@WebMethod
 	public List<ValueListImpl> atomicScan(KeyImpl begin, KeyImpl end,
 			Predicate<ValueListImpl> p) throws IOException,
 			BeginGreaterThanEndException, ServiceNotInitializedException {
-		// TODO Auto-generated method stub
-		return null;
+		return super.atomicScan(begin, end, p);
 	}
 
 	@Override
+	@WebMethod
 	public void bulkPut(List<Pair<KeyImpl, ValueListImpl>> mappings)
 			throws IOException, ServiceNotInitializedException {
-		// TODO Auto-generated method stub
-		
+		super.bulkPut(mappings);
 	}
 
 }
