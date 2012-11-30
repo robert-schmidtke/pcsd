993996ffa21ccafda4decf34513bf9fd8894fe71
Marcus <marcus@marcus-X1.(none)>
2012-12-01 00:03:21 +0100

    test-client, mit Zipf-Generator, und pyplot visualisierung

---
+++ assignment-1/test-client/Main.java
@@ -0,0 +1,30 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
+import test.ZipfGenerator;
+
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Random rand = new Random();
+		long COUNT = 10000;
+		
+        try {
+        	PrintWriter out = new PrintWriter("test.txt");
+            for (long i = 0; i < COUNT; i++) {
+            	double u = rand.nextDouble();
+        		ZipfGenerator zg = new ZipfGenerator(COUNT, 0.8);
+        		long rank = zg.calculateZipf(u);
+                out.println(rank);
+            }
+            out.close();
+        } catch (IOException e) {}
+	}
+
+}
