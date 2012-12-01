4995e29139a328b413ad64db8d4b4075b0912a21
Robert Schmidtke <ro.schmidtke@gmail.com>
2012-12-01 01:05:29 +0100

    recreated client after accidental deletion -- sorry :)

---
+++ assignment-1/client/src/dk/diku/pcsd/assignment1/impl/client/Client.java
@@ -0,0 +1,13 @@
+package dk.diku.pcsd.assignment1.impl.client;
+
+public class Client {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
