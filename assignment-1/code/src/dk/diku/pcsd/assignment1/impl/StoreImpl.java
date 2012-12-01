e02890f715d6956faf4ca3cd71e284683bdaa47a
Robert Schmidtke <ro.schmidtke@gmail.com>
2012-12-01 16:41:08 +0100

    removed logging; made exceptions more verbose; creating 1MB store.mmf if it does not exist already and put it in the tmp folder

--- assignment-1/code/src/dk/diku/pcsd/assignment1/impl/StoreImpl.java
+++ assignment-1/code/src/dk/diku/pcsd/assignment1/impl/StoreImpl.java
@@ -1,47 +1,64 @@
 package dk.diku.pcsd.assignment1.impl;
 
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.File;
 import java.io.RandomAccessFile;
 import java.nio.channels.FileChannel;
 
 import dk.diku.pcsd.keyvaluebase.interfaces.MemoryMappedFile;
 import dk.diku.pcsd.keyvaluebase.interfaces.Store;
 
-public class StoreImpl implements Store
-{
+public class StoreImpl implements Store {
 	
+	// size of the file if it does not exist already
+	private static final long MMF_INITIAL_SIZE = 1048576; // 1MB
+	
+	// store it in the system's temporary folder
+	private static final String MMF_PATH =
+			System.getProperty("java.io.tmpdir").endsWith(File.separator) ?
+					(System.getProperty("java.io.tmpdir") + "store.mmf") :
+					(System.getProperty("java.io.tmpdir") + File.separator + "store.mmf");
+	
+	// the actual file
+	private final RandomAccessFile mmfRandomAccessFile;
+	
+	// abstraction over the file
+	private final MemoryMappedFile mmf;
+	
+	// singleton
 	private static StoreImpl instance = null;
 	
-	private final PrintWriter logFileWriter;
-	
-	private final long MMF_SIZE = 1024 * 1024;
-	private final MemoryMappedFile mmf;
-	private final RandomAccessFile mmfBase;
-		
 	public static StoreImpl getInstance() {
 		if(instance == null)
 				instance = new StoreImpl();
 		return instance;
 	}
-	
+		
 	private StoreImpl() {
 		try {
-			logFileWriter = new PrintWriter("store.log");
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			throw new RuntimeException("Error creating log file for StoreImpl");
-		}
-		
-		try {
-			mmfBase = new RandomAccessFile("store.mmf", "rw");
-			mmfBase.setLength(MMF_SIZE);
-			mmf = new MemoryMappedFile(mmfBase.getChannel(), FileChannel.MapMode.READ_WRITE, 0, MMF_SIZE);
+			File mmfFile = new File(MMF_PATH);
+			if(mmfFile.exists())
+				mmfRandomAccessFile = new RandomAccessFile(mmfFile, "rw");
+			else {
+				// create if necessary
+				if(mmfFile.createNewFile()) {
+					mmfRandomAccessFile = new RandomAccessFile(mmfFile, "rw");
+					mmfRandomAccessFile.setLength(MMF_INITIAL_SIZE);
+				} else
+					throw new RuntimeException("Could not create " + MMF_PATH);
+			}
+			
+			// initialize the memory mapped file with either the old file or the newly created one
+			mmf = new MemoryMappedFile(mmfRandomAccessFile.getChannel(), FileChannel.MapMode.READ_WRITE, 0, mmfRandomAccessFile.length());
 		} catch (Exception e) {
-			e.printStackTrace(logFileWriter);
-			logFileWriter.flush();
 			throw new RuntimeException(e.getMessage(), e);
 		}
+	}
+	
+	@Override
+	protected void finalize() throws Throwable {
+		// close the raf on garbace collection
+		mmfRandomAccessFile.close();
+		super.finalize();		
 	}
 
 	@Override
@@ -51,9 +68,7 @@ public class StoreImpl implements Store
 			mmf.get(dst, position);
 			return dst;
 		} catch(Exception e) {
-			e.printStackTrace(logFileWriter);
-			logFileWriter.flush();
-			throw new RuntimeException(e.getMessage(), e);
+			throw new RuntimeException("Position: " + position + ", Length: " + length + ", " + e.getMessage(), e);
 		}
 	}
 
@@ -62,8 +77,6 @@ public class StoreImpl implements Store
 		try {
 			mmf.put(value, position);
 		} catch(Exception e) {
-			e.printStackTrace(logFileWriter);
-			logFileWriter.flush();
 			throw new RuntimeException("Position: " + position + ", Length: " + value.length + ", " + e.getMessage(), e);
 		}
 	}
