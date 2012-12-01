e1dd75ee252919549c2d82be3ab62778d16acb18
Robert Schmidtke <ro.schmidtke@gmail.com>
2012-12-01 16:58:25 +0100

    versioning the store.mmf file in the tmp dir using the package name

--- assignment-1/code/src/dk/diku/pcsd/assignment1/impl/StoreImpl.java
+++ assignment-1/code/src/dk/diku/pcsd/assignment1/impl/StoreImpl.java
@@ -1,6 +1,7 @@
 package dk.diku.pcsd.assignment1.impl;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.channels.FileChannel;
 
@@ -11,12 +12,6 @@ public class StoreImpl implements Store {
 	
 	// size of the file if it does not exist already
 	private static final long MMF_INITIAL_SIZE = 1048576; // 1MB
-	
-	// store it in the system's temporary folder
-	private static final String MMF_PATH =
-			System.getProperty("java.io.tmpdir").endsWith(File.separator) ?
-					(System.getProperty("java.io.tmpdir") + "store.mmf") :
-					(System.getProperty("java.io.tmpdir") + File.separator + "store.mmf");
 	
 	// the actual file
 	private final RandomAccessFile mmfRandomAccessFile;
@@ -34,22 +29,29 @@ public class StoreImpl implements Store {
 	}
 		
 	private StoreImpl() {
+		String tmpDir = System.getProperty("java.io.tmpdir");
+		if(!tmpDir.endsWith(File.separator))
+			tmpDir += File.separator;
+		
+		// versioning of the store
+		String mmfPath = tmpDir + getClass().getPackage().getName().replaceAll("\\.", File.separator) + File.separator + "store.mmf";
+		File mmfFile = new File(mmfPath);
+
 		try {
-			File mmfFile = new File(MMF_PATH);
 			if(mmfFile.exists())
 				mmfRandomAccessFile = new RandomAccessFile(mmfFile, "rw");
 			else {
 				// create if necessary
-				if(mmfFile.createNewFile()) {
+				if(mmfFile.getParentFile().mkdirs() & mmfFile.createNewFile()) {
 					mmfRandomAccessFile = new RandomAccessFile(mmfFile, "rw");
 					mmfRandomAccessFile.setLength(MMF_INITIAL_SIZE);
 				} else
-					throw new RuntimeException("Could not create " + MMF_PATH);
+					throw new RuntimeException("Could not create " + mmfPath);
 			}
 			
 			// initialize the memory mapped file with either the old file or the newly created one
 			mmf = new MemoryMappedFile(mmfRandomAccessFile.getChannel(), FileChannel.MapMode.READ_WRITE, 0, mmfRandomAccessFile.length());
-		} catch (Exception e) {
+		} catch (IOException e) {
 			throw new RuntimeException(e.getMessage(), e);
 		}
 	}
