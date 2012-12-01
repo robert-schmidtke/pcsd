62c3d6c32a82c9ab22d9fbb7e6c19a0824f0b0c7
Robert Schmidtke <ro.schmidtke@gmail.com>
2012-12-01 01:11:10 +0100

    inserting and reading key

--- assignment-1/client/src/dk/diku/pcsd/assignment1/impl/client/Client.java
+++ assignment-1/client/src/dk/diku/pcsd/assignment1/impl/client/Client.java
@@ -1,13 +1,36 @@
 package dk.diku.pcsd.assignment1.impl.client;
 
+import dk.diku.pcsd.assignment1.impl.KeyImpl;
+import dk.diku.pcsd.assignment1.impl.KeyValueBaseImplService;
+import dk.diku.pcsd.assignment1.impl.KeyValueBaseImplServiceService;
+import dk.diku.pcsd.assignment1.impl.ValueImpl;
+import dk.diku.pcsd.assignment1.impl.ValueListImpl;
+
 public class Client {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		KeyValueBaseImplServiceService kvbiss = new KeyValueBaseImplServiceService();
+		KeyValueBaseImplService kvbis = kvbiss.getKeyValueBaseImplServicePort();
+		
+		KeyImpl key = new KeyImpl();
+		key.setKey("testKey");
+		
+		ValueImpl value = new ValueImpl();
+		value.setValue("testValue");
+		
+		ValueListImpl valueList = new ValueListImpl();
+		valueList.getValueList().add(value);
+		
+		try {
+			kvbis.init("");
+			kvbis.insert(key, valueList);
+			System.out.println(kvbis.read(key).getValueList().get(0).getValue());
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
 	}
 
 }
