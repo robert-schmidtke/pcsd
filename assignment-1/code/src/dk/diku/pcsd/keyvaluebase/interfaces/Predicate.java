f6b5b40f8fb823b6770463bf7296bb9eec4c7432
Marco <marco@marco-ThinkPad-T61>
2012-12-04 20:07:23 +0100

    Added another predicate just for the fun of it

--- assignment-1/code/src/dk/diku/pcsd/keyvaluebase/interfaces/Predicate.java
+++ assignment-1/code/src/dk/diku/pcsd/keyvaluebase/interfaces/Predicate.java
@@ -2,9 +2,10 @@ package dk.diku.pcsd.keyvaluebase.interfaces;
 
 import javax.xml.bind.annotation.XmlSeeAlso;
 
+import dk.diku.pcsd.assignment1.impl.ListPredicate;
 import dk.diku.pcsd.assignment1.impl.StringLengthPredicate;
 
-@XmlSeeAlso({StringLengthPredicate.class})
+@XmlSeeAlso({StringLengthPredicate.class, ListPredicate.class})
 public abstract class Predicate<T> {
 	public abstract boolean evaluate(T input);
 }
