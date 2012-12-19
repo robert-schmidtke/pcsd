0235860a73090d9157875303c2b471b670a40392
Marco <marco@marco-ThinkPad-T61>
2012-12-19 22:36:44 +0100

    Forgot Replica

---
+++ assignment-3/proxy/src/dk/diku/pcsd/assignment3/proxy/impl/Replica.java
@@ -0,0 +1,84 @@
+package dk.diku.pcsd.assignment3.proxy.impl;
+
+import javax.jws.WebParam;
+
+import dk.diku.pcsd.assignment3.impl.KeyImpl;
+import dk.diku.pcsd.assignment3.master.impl.KeyValueBaseMasterImplService;
+import dk.diku.pcsd.assignment3.slave.impl.BeginGreaterThanEndException_Exception;
+import dk.diku.pcsd.assignment3.slave.impl.IOException_Exception;
+import dk.diku.pcsd.assignment3.slave.impl.KeyNotFoundException_Exception;
+import dk.diku.pcsd.assignment3.slave.impl.KeyValueBaseSlaveImplService;
+import dk.diku.pcsd.assignment3.slave.impl.ServiceNotInitializedException_Exception;
+import dk.diku.pcsd.keyvaluebase.interfaces.Pair;
+import dk.diku.pcsd.keyvaluebase.interfaces.Predicate;
+
+public class Replica {
+	private KeyValueBaseSlaveImplService slave;
+	private KeyValueBaseMasterImplService master;
+	private boolean hasMaster;
+
+	public Replica(KeyValueBaseMasterImplService m) {
+		this.master = m;
+		hasMaster = true;
+	}
+
+	public Replica(KeyValueBaseSlaveImplService s) {
+		this.slave = s;
+		hasMaster = false;
+	}
+
+	public Pair read(KeyImpl key) throws IOException_Exception,
+			KeyNotFoundException_Exception,
+			ServiceNotInitializedException_Exception {
+		if (hasMaster) {
+			try {
+				return master.read(key);
+			} catch (dk.diku.pcsd.assignment3.master.impl.IOException_Exception e) {
+				throw new IOException_Exception(e.getMessage(), null);
+			} catch (dk.diku.pcsd.assignment3.master.impl.KeyNotFoundException_Exception e) {
+				throw new KeyNotFoundException_Exception(e.getMessage(), null);
+			} catch (dk.diku.pcsd.assignment3.master.impl.ServiceNotInitializedException_Exception e) {
+				throw new ServiceNotInitializedException_Exception(
+						e.getMessage(), null);
+			}
+		} else {
+			return slave.read(key);
+		}
+	}
+
+	public Pair scan(KeyImpl from, KeyImpl to, Predicate pred)
+			throws BeginGreaterThanEndException_Exception,
+			IOException_Exception, ServiceNotInitializedException_Exception {
+		if (hasMaster){
+			try {
+				return master.scan(from, to, pred);
+			} catch (dk.diku.pcsd.assignment3.master.impl.BeginGreaterThanEndException_Exception e) {
+				throw new BeginGreaterThanEndException_Exception(e.getMessage(), null);
+			} catch (dk.diku.pcsd.assignment3.master.impl.IOException_Exception e) {
+				throw new IOException_Exception(e.getMessage(), null);
+			} catch (dk.diku.pcsd.assignment3.master.impl.ServiceNotInitializedException_Exception e) {
+				throw new ServiceNotInitializedException_Exception(e.getMessage(), null);
+			}
+		}else{
+			return slave.scan(from, to, pred);
+		}
+	}
+	
+	public Pair atomicScan(KeyImpl from, KeyImpl to, Predicate pred)
+			throws BeginGreaterThanEndException_Exception,
+			IOException_Exception, ServiceNotInitializedException_Exception {
+		if (hasMaster){
+			try {
+				return master.atomicScan(from, to, pred);
+			} catch (dk.diku.pcsd.assignment3.master.impl.BeginGreaterThanEndException_Exception e) {
+				throw new BeginGreaterThanEndException_Exception(e.getMessage(), null);
+			} catch (dk.diku.pcsd.assignment3.master.impl.IOException_Exception e) {
+				throw new IOException_Exception(e.getMessage(), null);
+			} catch (dk.diku.pcsd.assignment3.master.impl.ServiceNotInitializedException_Exception e) {
+				throw new ServiceNotInitializedException_Exception(e.getMessage(), null);
+			}
+		}else{
+			return slave.atomicScan(from, to, pred);
+		}
+	}
+}
