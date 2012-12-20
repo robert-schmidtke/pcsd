e28bd2382e874a2268f63c25e4b947ee890ee561
Marco <marco@marco-ThinkPad-T61>
2012-12-20 02:54:26 +0100

    MultiReadWroteTest uses proxy, added wsimport howto to todo file

--- assignment-3/test-client/src/test/MultiReadWriteTest.java
+++ assignment-3/test-client/src/test/MultiReadWriteTest.java
@@ -11,109 +11,119 @@ import java.util.concurrent.Executors;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import dk.diku.pcsd.assignment3.master.impl.FileNotFoundException_Exception;
-import dk.diku.pcsd.assignment3.master.impl.IOException_Exception;
-import dk.diku.pcsd.assignment3.master.impl.KeyAlreadyPresentException_Exception;
-import dk.diku.pcsd.assignment3.master.impl.KeyImpl;
-import dk.diku.pcsd.assignment3.master.impl.KeyNotFoundException_Exception;
-import dk.diku.pcsd.assignment3.master.impl.KeyValueBaseMasterImplService;
-import dk.diku.pcsd.assignment3.master.impl.KeyValueBaseMasterImplServiceService;
-import dk.diku.pcsd.assignment3.master.impl.ServiceAlreadyInitializedException_Exception;
-import dk.diku.pcsd.assignment3.master.impl.ServiceInitializingException_Exception;
-import dk.diku.pcsd.assignment3.master.impl.ServiceNotInitializedException_Exception;
-import dk.diku.pcsd.assignment3.master.impl.ValueImpl;
-import dk.diku.pcsd.assignment3.master.impl.ValueListImpl;
+import dk.diku.pcsd.assignment3.proxy.impl.Configuration;
+import dk.diku.pcsd.assignment3.proxy.impl.IOException_Exception;
+import dk.diku.pcsd.assignment3.proxy.impl.KeyAlreadyPresentException_Exception;
+import dk.diku.pcsd.assignment3.proxy.impl.KeyImpl;
+import dk.diku.pcsd.assignment3.proxy.impl.KeyNotFoundException_Exception;
+import dk.diku.pcsd.assignment3.proxy.impl.KeyValueBaseProxyImplService;
+import dk.diku.pcsd.assignment3.proxy.impl.KeyValueBaseProxyImplServiceService;
+import dk.diku.pcsd.assignment3.proxy.impl.ServiceAlreadyConfiguredException_Exception;
+import dk.diku.pcsd.assignment3.proxy.impl.ServiceNotInitializedException_Exception;
+import dk.diku.pcsd.assignment3.proxy.impl.ValueImpl;
+import dk.diku.pcsd.assignment3.proxy.impl.ValueListImpl;
 
 public class MultiReadWriteTest {
-	static KeyValueBaseMasterImplServiceService kvbiss;
-	static KeyValueBaseMasterImplService kvbis;
-	
+	static KeyValueBaseProxyImplServiceService kvbiss;
+	static KeyValueBaseProxyImplService kvbis;
+
 	static HashMap<String, String> testMap = new HashMap<String, String>();
 	static ArrayList<String> keys;
-	
+
 	static boolean testSuccessfull = true;
-	
-	
-	@BeforeClass 
-	public static void testSetup(){
-		kvbiss = new KeyValueBaseMasterImplServiceService();
-		kvbis = kvbiss.getKeyValueBaseMasterImplServicePort();
+
+	@BeforeClass
+	public static void testSetup() {
+		kvbiss = new KeyValueBaseProxyImplServiceService();
+		kvbis = kvbiss.getKeyValueBaseProxyImplServicePort();
+		Configuration conf = new Configuration();
+		conf.setMaster("http://localhost:8080/master/keyvaluebasemaster?wsdl");
+		conf.getSlaves().add("http://localhost:8080/slave/keyvaluebaseslave?wsdl");
+
 		try {
+			kvbis.config(conf);
 			kvbis.init("");
-		} catch (FileNotFoundException_Exception e) {
+		} catch (dk.diku.pcsd.assignment3.proxy.impl.FileNotFoundException_Exception e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (ServiceAlreadyInitializedException_Exception e) {
+		} catch (dk.diku.pcsd.assignment3.proxy.impl.ServiceAlreadyInitializedException_Exception e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (ServiceInitializingException_Exception e) {
+		} catch (dk.diku.pcsd.assignment3.proxy.impl.ServiceInitializingException_Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ServiceAlreadyConfiguredException_Exception e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
 	}
-	
+
 	@Test
 	public void parallelRead() {
-		//how often before
+		// how often before
 		int N = 1000;
-		
-		//how many threads
+
+		// how many threads
 		int h = 10;
-		//how many writes per thread (makes 10 reads per write))
+		// how many writes per thread (makes 10 reads per write))
 		int n = 100;
-		
-		Random rnd = new Random();		
-		
-		//write N key values in hashmap and store
-		for (int i=0; i<N; i++) {
-				String keyValue = String.valueOf(rnd.nextInt(999999));
-				String resultValue = String.valueOf(rnd.nextInt(99999));
-				
-				KeyImpl key = new KeyImpl();
-				key.setKey(keyValue);
-				
-				ValueImpl value = new ValueImpl();
-				value.setValue(resultValue);
-				
-				ValueListImpl valueList = new ValueListImpl();
-				valueList.getValueList().add(value);
-				try{
-				
+
+		Random rnd = new Random();
+
+		// write N key values in hashmap and store
+		for (int i = 0; i < N; i++) {
+			String keyValue = String.valueOf(rnd.nextInt(999999));
+			String resultValue = String.valueOf(rnd.nextInt(99999));
+
+			KeyImpl key = new KeyImpl();
+			key.setKey(keyValue);
+
+			ValueImpl value = new ValueImpl();
+			value.setValue(resultValue);
+
+			ValueListImpl valueList = new ValueListImpl();
+			valueList.getValueList().add(value);
+			try {
+
 				kvbis.insert(key, valueList);
 				testMap.put(keyValue, resultValue);
-				
-				} catch (KeyAlreadyPresentException_Exception e) {
-					//do nothing
-				} catch (IOException_Exception e) {
-					e.printStackTrace();
-				} catch (ServiceNotInitializedException_Exception e) {
-					e.printStackTrace();
-				}
-			}
-			
-			keys = new ArrayList<String>(testMap.keySet());
-		
-			//make threads and let them read and write
-			ExecutorService executor = Executors.newFixedThreadPool(h);
-		    for (int i = 0; i < h; i++) {
-		      Runnable worker = new ReadThread(n);
-		      executor.execute(worker);
-		    }
-		    // This will make the executor accept no new threads
-		    // and finish all existing threads in the queue
-		    executor.shutdown();
-		    // Wait until all threads are finish
-		    while (!executor.isTerminated()) {
 
-		    }
-		    		
-		    //if one read failed fail test case
-		    assertTrue("Result", testSuccessfull);
-		
+			} catch (KeyAlreadyPresentException_Exception e) {
+				// do nothing
+			} catch (IOException_Exception e) {
+				e.printStackTrace();
+			} catch (ServiceNotInitializedException_Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		keys = new ArrayList<String>(testMap.keySet());
+
+		// make threads and let them read and write
+		ExecutorService executor = Executors.newFixedThreadPool(h);
+		for (int i = 0; i < h; i++) {
+			Runnable worker = new ReadThread(n);
+			executor.execute(worker);
+		}
+		// This will make the executor accept no new threads
+		// and finish all existing threads in the queue
+		executor.shutdown();
+		// Wait until all threads are finish
+		while (!executor.isTerminated()) {
+
+		}
+
+		// if one read failed fail test case
+		assertTrue("Result", testSuccessfull);
+
 	}
-	
+
 	public static class ReadThread implements Runnable {
 		int n;
-		
+
 		ReadThread(int n) {
-			  this.n = n;
+			this.n = n;
 		}
 
 		@Override
@@ -121,59 +131,61 @@ public class MultiReadWriteTest {
 			Random rnd = new Random();
 			String actualValue = "";
 			String expectedValue = "";
-			
-			for (int i=0;i<this.n;i++){
-					//write here
-					String keyValue = String.valueOf(rnd.nextInt(999999));
-					String resultValue = String.valueOf(rnd.nextInt(99999));
-					
-					KeyImpl key = new KeyImpl();
-					key.setKey(keyValue);
-					
-					ValueImpl value = new ValueImpl();
-					value.setValue(resultValue);
-					
-					ValueListImpl valueList = new ValueListImpl();
-					valueList.getValueList().add(value);
-					try{
+
+			for (int i = 0; i < this.n; i++) {
+				// write here
+				String keyValue = String.valueOf(rnd.nextInt(999999));
+				String resultValue = String.valueOf(rnd.nextInt(99999));
+
+				KeyImpl key = new KeyImpl();
+				key.setKey(keyValue);
+
+				ValueImpl value = new ValueImpl();
+				value.setValue(resultValue);
+
+				ValueListImpl valueList = new ValueListImpl();
+				valueList.getValueList().add(value);
+				try {
 
 					kvbis.insert(key, valueList);
 					testMap.put(keyValue, resultValue);
-					
-					} catch (KeyAlreadyPresentException_Exception e) {
-						//do nothing
+
+				} catch (KeyAlreadyPresentException_Exception e) {
+					// do nothing
+				} catch (IOException_Exception e) {
+					e.printStackTrace();
+				} catch (ServiceNotInitializedException_Exception e) {
+					e.printStackTrace();
+				}
+
+				// read here
+				for (int j = 0; j < 10; j++) {
+					try {
+						String randomReadKey = keys
+								.get(rnd.nextInt(keys.size()));
+
+						KeyImpl keyRead = new KeyImpl();
+						keyRead.setKey(randomReadKey);
+
+						expectedValue = testMap.get(randomReadKey);
+						actualValue = kvbis.read(keyRead).getValueList().get(0).getValue().toString();
+								
 					} catch (IOException_Exception e) {
+						e.printStackTrace();
+					} catch (KeyNotFoundException_Exception e) {
 						e.printStackTrace();
 					} catch (ServiceNotInitializedException_Exception e) {
 						e.printStackTrace();
-					}		
-					
-					//read here
-					for (int j=0; j<10; j++){
-						try{
-						String randomReadKey = keys.get(rnd.nextInt(keys.size()));
-						
-						KeyImpl keyRead = new KeyImpl();
-						keyRead.setKey(randomReadKey);
-						
-						expectedValue = testMap.get(randomReadKey);
-						actualValue = ((ValueListImpl)kvbis.read(keyRead).getV()).getValueList().get(0).getValue().toString();
-						} catch (IOException_Exception e) {
-							e.printStackTrace();
-						} catch (KeyNotFoundException_Exception e) {
-							e.printStackTrace();
-						} catch (ServiceNotInitializedException_Exception e) {
-							e.printStackTrace();
-						}
-						
-						if (!expectedValue.equals(actualValue)) {
-							testSuccessfull = false;
-							System.out.println("Fehler: " + expectedValue + " " + actualValue);
-						}
 					}
-				
-				
-			}	
+
+					if (!expectedValue.equals(actualValue)) {
+						testSuccessfull = false;
+						System.out.println("Fehler: " + expectedValue + " "
+								+ actualValue);
+					}
+				}
+
+			}
 		}
-	} 
+	}
 }
